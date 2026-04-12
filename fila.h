c63a859e8cff58f098b5b76ecfb0685fdc45ea92
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2026-04-12 17:14:07 -0300

    Update fila functions to void return type
    Changed return types of fila_destroi and fila_insere functions to void.

--- fila.h
+++ fila.h
@@ -22,19 +22,15 @@ struct fila_t *fila_cria ();
 
 /* libera todas as estruturas de dados da fila, inclusive os itens. */
 /* retorna NULL */
-struct fila_t *fila_destroi (struct fila_t *f);
+void fila_destroi (struct fila_t *f);
 
 /* insere um nodo no final da fila seguindo a politica FIFO. */
-/* retorna 1 se tiver sucesso ou 0 se falhar. */
-int fila_insere (struct fila_t *f, struct nodofila *item);
+void fila_insere (struct fila_t *f, struct nodofila *item);
 
 /* retira o primeiro nodo da fila e o retorna */
 struct nodo *fila_retira (struct fila_t *f);
 
 /* retorna o número de itens na fila ou -1 em caso de erro. */
 int fila_tamanho (struct fila_t *f);
-
-/* imprime o conteúdo da fila */
-void fila_imprime (struct fila_t *f);
 
 #endif
