b49b4e5a7ffdba9983fc0ea22f59e8649059a402
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2026-04-12 19:13:49 -0300

    renomeae

--- fila.h
+++ fila.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* representa cada elemento da fila. */
+/* contém cada nodo como item e um ponteiro para o próximo elemento */
 struct nodofila {
         struct nodo *item;
         struct nodofila *prox;
@@ -17,19 +19,16 @@ struct fila_t
 };
 
 /* cria uma fila vazia, retornando um ponteiro para a fila criada ou NULL se erro. */
-struct fila_t *fila_cria ();
+struct fila_t *criarFila ();
 
 /* libera todas as estruturas de dados da fila, inclusive os itens. */
 /* retorna NULL */
-void fila_destroi (struct fila_t *f);
+void destruirFila (struct fila_t *f);
 
 /* insere um nodo no final da fila seguindo a politica FIFO. */
-void fila_insere (struct fila_t *f, struct nodo *item);
+void inserirFila (struct fila_t *f, struct nodo *item);
 
 /* retira o primeiro nodo da fila e o retorna */
-struct nodo *fila_retira (struct fila_t *f);
-
-/* retorna o número de itens na fila ou -1 em caso de erro. */
-int fila_tamanho (struct fila_t *f);
+struct nodo *retirarFila (struct fila_t *f);
 
 #endif
