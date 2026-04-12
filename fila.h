3a5c4b0541fdbf368b355323d5ccd4549899aa2f
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2026-04-11 23:42:20 -0300

    Refactor fila structure to use nodofila

--- fila.h
+++ fila.h
@@ -4,10 +4,16 @@
 #include <stdlib.h>
 #include "arvoreB.h"
 
+struct nodofila {
+        struct nodo *no;
+        struct nodofila *prox;
+}
+
 /* descreve uma fila */
 struct fila_t
 {
-        struct nodo **nodos; /* vetor de nodos */            
+        struct nodofila *prim;            
+        struct nodofila *ult; 
         int num; /* número de itens na fila */
 };
 
@@ -20,10 +26,10 @@ struct fila_t *fila_destroi (struct fila_t *f);
 
 /* insere um nodo no final da fila seguindo a politica FIFO. */
 /* retorna 1 se tiver sucesso ou 0 se falhar. */
-int fila_insere (struct fila_t *f, struct nodo *item);
+int fila_insere (struct fila_t *f, struct nodofila *item);
 
 /* retira o primeiro nodo da fila e o devolve, retornando 1 em sucesso e 0 c.c. */
-int fila_retira (struct fila_t *f, struct nodo *item);
+int fila_retira (struct fila_t *f, struct nodofila *item);
 
 /* retorna o número de itens na fila ou -1 em caso de erro. */
 int fila_tamanho (struct fila_t *f);
