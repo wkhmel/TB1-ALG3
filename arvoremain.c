024736b4603b11123fd68ea04c4281cd32e4bcc9
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2026-04-14 10:06:00 -0300

    estilo kernel

--- arvoremain.c
+++ arvoremain.c
@@ -4,39 +4,44 @@
 
 int main() 
 {
-        int32_t t;
+        int32_t t, x, indice;
+        struct arvoreB *arv;
+        
+        printf("Insira um valor para t (minimo 2):\n");
         scanf("%d", &t);
-        printf("\n");
-
-        struct arvoreB *arv = criarArvoreB(t);
-  
-        int32_t x;
+        
+        arv = criarArvoreB(t);
+        if (!arv)
+                return 1;
+        
         do {
                 printf("Insira um elemento: ");
                 scanf("%d", &x);
+                if (x = -1)
+                        break;
                 printf(" ");
                 inserirArvoreB(arv, x);
                 imprimirArvoreB(arv);
+                printf("\n");
                 imprimirEmOrdem(arv);
                 printf("\n");
         } while (x != -1);
 
         printf("Qual elemento deseja buscar? ");
+
         scanf("%d", &x);
+
+        if (x < 0)
+                return 1;
+        
         printf("\n");
 
-        int32_t *indice = malloc(sizeof(int32_t));
-        
-        if (!indice) {
-                fprintf("Falha ao alocar memoria.\n");
-                exit(1);
-        }
-
-        if (BuscarArvoreB(arv, x, indice)) {
+        if (BuscarArvoreB(arv, x, &indice)) {
                 printf("A chave %d foi encontrada com sucesso e esta no indice %d de seu nodo.", x, indice);
+        } else {
+                printf("A chave %d nao foi encontrada.", x);
         }
 
-        free(indice);
         deletarArvore(arv);
   
         return 0;
