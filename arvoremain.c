f8cb25fa8e2c56f6abe2708948b59ade432df37a
Melissa Goulart Kemp <mgk25@inf.ufpr.br>
2026-04-14 09:53:24 -0300

    edições

--- arvoremain.c
+++ arvoremain.c
@@ -2,24 +2,42 @@
 #include <stdlib.h>
 #include "arvoreB.h"
 
-int main() {
-    int32_t t;
-    scanf("%d", &t);
-    printf("\n");
-
-  struct arvoreB *arv = criarArvoreB(t);
-  
-    int32_t x;
-    do {
-        scanf("%d", &x);
-        printf(" ");
-        inserirArvoreB(arv, x);
-        imprimirArvoreB(arv);
-        imprimirEmOrdem(arv);
+int main() 
+{
+        int32_t t;
+        scanf("%d", &t);
         printf("\n");
-    } while (x != -1);
 
-    deletarArvore(arv);
+        struct arvoreB *arv = criarArvoreB(t);
   
-    return 0;
+        int32_t x;
+        do {
+                printf("Insira um elemento: ");
+                scanf("%d", &x);
+                printf(" ");
+                inserirArvoreB(arv, x);
+                imprimirArvoreB(arv);
+                imprimirEmOrdem(arv);
+                printf("\n");
+        } while (x != -1);
+
+        printf("Qual elemento deseja buscar? ");
+        scanf("%d", &x);
+        printf("\n");
+
+        int32_t *indice = malloc(sizeof(int32_t));
+        
+        if (!indice) {
+                fprintf("Falha ao alocar memoria.\n");
+                exit(1);
+        }
+
+        if (BuscarArvoreB(arv, x, indice)) {
+                printf("A chave %d foi encontrada com sucesso e esta no indice %d de seu nodo.", x, indice);
+        }
+
+        free(indice);
+        deletarArvore(arv);
+  
+        return 0;
 }
